f78aa62f9127bd3f845fb088b51bc593be264494
sasszz <luchevreuil@gmail.com>
2022-08-01 19:43:00 -0700

    nested dictionaries assignment second commit

--- assignments/nested_dictionaries_and_lists.py
+++ assignments/nested_dictionaries_and_lists.py
@@ -1,5 +1,6 @@
+########################################## PART ONE ###########################################
 x = [
-    [5, 2, 3], 
+    [5, 2, 3],
     [10, 8, 9]
 ]
 students = [
@@ -13,18 +14,101 @@ sports_directory = {
 z = [
     {'x': 10, 'y': 20}]
 
-# Change the value 10 in x to 15. Once you're done, x should now be [ [5,2,3], [15,8,9] ].
+# # Change the value 10 in x to 15. Once you're done, x should now be [ [5,2,3], [15,8,9] ].
 x[1][0] = 15
 print(x)
 
-# Change the last_name of the first student from 'Jordan' to 'Bryant'
+# # Change the last_name of the first student from 'Jordan' to 'Bryant'
 students[0]['last_name'] = 'Bryant'
 print(students)
 
-# In the sports_directory, change 'Messi' to 'Andres'
+# # In the sports_directory, change 'Messi' to 'Andres'
 sports_directory['soccer'][0] = 'Andres'
 print(sports_directory)
 
-# Change the value 20 in z to 30
+# # Change the value 20 in z to 30
 z[0]['y'] = 30
 print(z)
+
+# ########################################## PART TWO ###########################################
+students = [
+    {'first_name':  'Michael', 'last_name': 'Jordan'},
+    {'first_name': 'John', 'last_name': 'Rosales'},
+    {'first_name': 'Mark', 'last_name': 'Guillen'},
+    {'first_name': 'KB', 'last_name': 'Tonel'}
+]
+
+def iterate_dictionary(some_list):
+    for i in range(0, len(some_list)):
+        print(some_list[i])
+
+print(iterate_dictionary(students))
+
+# should output: (it's okay if each key-value pair ends up on 2 separate lines;
+# bonus to get them to appear exactly as below!)
+# first_name - Michael, last_name - Jordan
+# first_name - John, last_name - Rosales
+# first_name - Mark, last_name - Guillen
+# first_name - KB, last_name - Tonel
+
+########################################## PART THREE ###########################################
+# Create a function iterateDictionary2(key_name, some_list) that, given a list of dictionaries and a key name, the function prints the value stored in that key for each dictionary. For example, iterateDictionary2('first_name', students) should output:
+# For example:
+# first_name, students
+# Michael
+# John
+# Mark
+# KB
+
+students = [
+    {'first_name':  'Michael', 'last_name': 'Jordan'},
+    {'first_name': 'John', 'last_name': 'Rosales'},
+    {'first_name': 'Mark', 'last_name': 'Guillen'},
+    {'first_name': 'KB', 'last_name': 'Tonel'}
+]
+
+def iterate_dictionary2(key, list):
+    for i in range(0,len(list)):
+        print(list[i][key])
+
+
+iterate_dictionary2('first_name', students)
+
+########################################## PART FOUR ###########################################
+# Create a function printInfo(some_dict) that given a dictionary whose values are all lists, prints the name of each key along with the size of its list, and then prints the associated values within each key's list. 
+
+dojo = {
+    'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
+    'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
+}
+
+
+def print_info(some_dict):
+    for key in some_dict:
+        print(f"{len(some_dict[key])} {key.upper()}")
+        for item in some_dict[key]:
+            print(item)
+
+
+print(print_info(dojo))
+
+
+# output:
+# 7 LOCATIONS
+# San Jose
+# Seattle
+# Dallas
+# Chicago
+# Tulsa
+# DC
+# Burbank
+
+# 8 INSTRUCTORS
+# Michael
+# Amy
+# Eduardo
+# Josh
+# Graham
+# Patrick
+# Minh
+# Devon
