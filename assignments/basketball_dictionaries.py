2e1f11c7b41f382ef5961b9c2c1db3ef096dc452
sasszz <luchevreuil@gmail.com>
2022-08-03 10:43:30 -0700

    Challenge 3 completed for Bball Dictionaries

--- assignments/basketball_dictionaries.py
+++ assignments/basketball_dictionaries.py
@@ -43,20 +43,31 @@ players = [
 # Challenge 1: Update the constructor to accept a dictionary with a single player's information instead of individual arguments for the attributes.
 
 class Player: #My attempt at Challenge #1 - it works!!
-    def __init__(self, some_list, i):
-        self.name = some_list[i]["name"]
-        self.age = some_list[i]["age"]
-        self.position = some_list[i]["position"]
-        self.team = some_list[i]["team"]
+    players = []
+
+    def __init__(self, data):
+        self.name = data["name"]
+        self.age = data["age"]
+        self.position = data["position"]
+        self.team = data["team"]
+
+    @classmethod # create a class method to import dictionary to class
+    def register(cls, some_list):
+        for dict in some_list:
+            cls.players.append(Player(dict)) # append dict to empty Players Array
 
 
 # CHALLENGE 2: Given these variables, create Player instances for each of the following dictionaries. Be sure to instantiate these outside the class definition, in the outer scope.
 
-kevin = Player(players, 0)
-print(kevin.name)
-jason = Player(players, 1)
-print(jason.name)
-kyrie = Player(players, 2)
-print(kyrie.name)
+# kevin = Player(players, 0)
+# print(kevin.name)
+# jason = Player(players, 1)
+# print(jason.name)
+# kyrie = Player(players, 2)
+# print(kyrie.name)
 
 # CHALLENGE 3: Finally, given the example list of players at the top of this module (the one with many players) write a for loop that will populate an empty list with Player objects from the original list of dictionaries.
+
+# see classmethod above
+player_instances = Player.register(players)
+print(players[0]) # this also works
