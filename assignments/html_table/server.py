84d1205ddc53a80aaba2b7efb6a87e9008bcdf65
sasszz <luchevreuil@gmail.com>
2022-08-04 16:31:55 -0700

    HTML Table Assignment

---
+++ assignments/html_table/server.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template
+
+# Create a new instance of the Flask class called 'app'
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'  # Return the string 'Hello World!' as a response
+
+
+@app.route('/html')
+def html():
+    users = [
+        {'first_name': 'Michael', 'last_name': 'Choi'},
+        {'first_name': 'John', 'last_name': 'Supsupin'},
+        {'first_name': 'Mark', 'last_name': 'Guillen'},
+        {'first_name': 'KB', 'last_name': 'Tonel'}
+    ]
+    return render_template("index.html", users = users)
+
+
+if __name__ == '__main__':   # Ensure this file is being run directly and not from a different module
+    app.run(debug=True)    # Run the app in debug mode
